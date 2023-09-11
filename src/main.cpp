5f2a210d736a79b2c2e3a9fa95a9169883f12d49
btjacobson <31464526+btjacobson@users.noreply.github.com>
2023-09-10 18:12:09 -0600

    Implementing basic DirectX

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <Windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
+// global declarations
+IDXGISwapChain* swapchain;             // the pointer to the swap chain interface
+ID3D11Device* dev;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext* devcon;           // the pointer to our Direct3D device context
+ID3D11RenderTargetView* backBuffer;
+
+// function prototypes
+void InitD3D(HWND hWnd);     // sets up and initializes Direct3D
+void RenderFrame();
+void CleanD3D();         // closes Direct3D and releases memorys
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
@@ -27,6 +46,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	ShowWindow(hWnd, nCmdShow);
 
+	InitD3D(hWnd);
+
 	MSG msg = { 0 };
 	while (TRUE)
 	{
@@ -44,9 +65,70 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		{
 
 		}
+
+		RenderFrame();
 	}
 
 	return msg.wParam;
+}
+
+void InitD3D(HWND hWnd)
+{
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.OutputWindow = hWnd;
+	swapChainDesc.SampleDesc.Count = 4;
+	swapChainDesc.Windowed = TRUE;
+
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&swapChainDesc,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+
+	ID3D11Texture2D* tBackBuffer;
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&tBackBuffer);
+
+	dev->CreateRenderTargetView(tBackBuffer, NULL, &backBuffer);
+	tBackBuffer->Release();
+
+	devcon->OMSetRenderTargets(1, &backBuffer, NULL);
+
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = 800;
+	viewport.Height = 600;
+
+	devcon->RSSetViewports(1, &viewport);
+}
+
+void RenderFrame()
+{
+	devcon->ClearRenderTargetView(backBuffer, D3DXCOLOR(0.22f, 0.22f, 0.22f, 1.0f));
+
+	swapchain->Present(0, 0);
+}
+
+void CleanD3D()
+{
+	swapchain->Release();
+	backBuffer->Release();
+	dev->Release();
+	devcon->Release();
 }
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
